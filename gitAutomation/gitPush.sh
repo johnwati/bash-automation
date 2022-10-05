22a7cbfd2d84bd40d7a6eb81501a83f74451c146
John Wati <john.wati.ke@gmail.com>
2022-10-05 11:56:21 +0300

    Initial commit

--- gitAutomation/gitPush.sh
+++ gitAutomation/gitPush.sh
@@ -1,1 +1,5 @@
-git init 
+git add .
+
+git commit -m"Initial commit"
+
+git push 
--- gitAutomation/gitSetup.sh
+++ gitAutomation/gitSetup.sh
@@ -1,3 +1,5 @@
 git init
 
 git remote add origin "git@github.com:johnwati/bash-automation.git"
+
+git push --set-upstream origin master
