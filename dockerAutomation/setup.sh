87f0a834c9f3156065b757f917da0cc744382fe3
John Wati <john.wati.ke@gmail.com>
2022-10-05 19:57:28 +0300

    upaded new repo

---
+++ dockerAutomation/setup.sh
@@ -0,0 +1,4 @@
+# docker login registry.gitlab.com
+docker build -t registry.gitlab.com/savoir-ms/mobifarm-ussd-service .
+
+docker push registry.gitlab.com/savoir-ms/mobifarm-ussd-service
--- gitAutomation/gitSetup.sh
+++ gitAutomation/gitSetup.sh
@@ -1,5 +1,22 @@
-git init
+# git init
 
-git remote add origin "git@github.com:johnwati/bash-automation.git"
+# git remote add origin "git@github.com:johnwati/bash-automation.git"
 
-git push --set-upstream origin main
+# git push --set-upstream origin main
+
+echo "Enter your message"
+read message
+git add .
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN l"
+ git push -u origin master
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
+echo "bye"
+git status
+read message
